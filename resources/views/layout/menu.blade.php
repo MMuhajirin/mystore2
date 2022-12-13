dde3e570dbb87ccecd4efb8702ca395acf2d5561
MMuhajirin <muhamadmuhajirin34@gmail.com>
2022-12-14 02:13:33 +0700

    login

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -7,6 +7,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
+                <a class="nav-link" href="{{ URL::to('login') }}">Login</a>
+                </li>
+          <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="{{ URL::to('homepage')}}">Menu</a>
           </li>
           <li class="nav-item">
